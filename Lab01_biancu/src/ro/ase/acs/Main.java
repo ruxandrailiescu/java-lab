3d345e1c6a2ed47cdbc08069be8d09cb41698c16
Ruxandra Iliescu <iliescuruxandra23@stud.ase.ro>
2024-06-23 06:06:51 +0300

    Lab01

--- Lab01_biancu/src/ro/ase/acs/Main.java
+++ Lab01_biancu/src/ro/ase/acs/Main.java
@@ -3,7 +3,34 @@ package ro.ase.acs;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello, World!");
+		
+		// in C++ s-ar fi chemat constructorul implicit
+		// in Java, clasele sunt de tip referential => se comporta ca un pointer
+		// => valoarea lui student este null
+		//Student student;
+		
+		// se apeleaza constructorul implicit
+		Student student = new Student();
+		System.out.println(student.getGrade());
+		
+		// 9.5 ar fi interpretat ca double => noi avem tip de data float
+		Student student2 = new Student("Ionel", 9.5f);
+		System.out.println(student2.getGrade());
+		
+		// shallow copy => ambele obiecte pointeaza spre aceeasi zona de memorie in heap
+		// => solutie: clonare
+		Student student3 = student2;
+		student3.setName("Gigel");
+		System.out.println(student2.getName());
+		
+		// constructorul de copiere nu este definit default ca in C++
+		
+		// deep copy
+		Student student4 = student2.myClone();
+		student4.setName("Alex");
+		System.out.println(student2.getName());
+		
 	}
 
 }
---
+++ Lab01_biancu/src/ro/ase/acs/Student.java
@@ -0,0 +1,39 @@
+package ro.ase.acs;
+
+public class Student {
+	private String name;
+	private float grade;
+	
+	public Student() {
+		name = "";
+		grade = 0;
+	}
+	
+	public Student(String name, float grade) {
+		this.name = name;
+		this.grade = grade;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getGrade() {
+		return grade;
+	}
+
+	public void setGrade(float grade) {
+		this.grade = grade;
+	}
+	
+	public Student myClone() {
+		Student copyStudent = new Student();
+		copyStudent.name = name;
+		copyStudent.grade = grade;
+		return copyStudent;
+	}
+}
